ef1f4d1c057af4faa4828ad7bf02f119a43a08f9
yangpa043 <yangpa043@gmail.com>
2020-12-27 03:38:32 +0900

    12.17

--- script1.js
+++ script1.js
@@ -1,3 +1,25 @@
 window.onload = function () {
+    // 주제박스 이벤트
+    var sbox = document.getElementById('sbox')
+    var gbox = document.getElementById('gbox')
+    var cbox = document.getElementById('cbox')
 
+    sbox.addEventListener('mouseover', function () {
+        sbox.style.backgroundColor = "rgb(230,230,230";
+    })
+    sbox.addEventListener('mouseout', function () {
+        sbox.style.backgroundColor = "white";
+    })
+    gbox.addEventListener('mouseover', function () {
+        gbox.style.backgroundColor = "rgb(230,230,230";
+    })
+    gbox.addEventListener('mouseout', function () {
+        gbox.style.backgroundColor = "white";
+    })
+    cbox.addEventListener('mouseover', function () {
+        cbox.style.backgroundColor = "rgb(230,230,230";
+    })
+    cbox.addEventListener('mouseout', function () {
+        cbox.style.backgroundColor = "white";
+    })
 }
--- script3.js
+++ script3.js
@@ -1,3 +1,16 @@
 window.onload = function () {
-
+    var contain = document.querySelector('.contain')
+    var page = 0
+    document.querySelector('.right').addEventListener('click', function () {
+        if (page > -6500) {
+            page = page - 500
+            contain.style.transform = 'translate(' + page + 'px)'
+        }
+    })
+    document.querySelector('.left').addEventListener('click', function () {
+        if (page < 0) {
+            page = page + 500
+            contain.style.transform = 'translate(' + page + 'px)'
+        }
+    })
 }
