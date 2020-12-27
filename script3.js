17e4eaf2c348fb119459d424731ddedb797e3f5d
yangpa043 <yangpa043@gmail.com>
2020-12-28 00:49:50 +0900

    12.28

--- script3.js
+++ script3.js
@@ -13,4 +13,18 @@ window.onload = function () {
             contain.style.transform = 'translate(' + page + 'px)'
         }
     })
+    var contain2 = document.querySelector('.contain2')
+    var page2 = 0
+    document.querySelector('.right2').addEventListener('click', function () {
+        if (page2 > -5000) {
+            page2 = page2 - 500
+            contain2.style.transform = 'translate(' + page2 + 'px)'
+        }
+    })
+    document.querySelector('.left2').addEventListener('click', function () {
+        if (page2 < 0) {
+            page2 = page2 + 500
+            contain2.style.transform = 'translate(' + page2 + 'px)'
+        }
+    })
 }
